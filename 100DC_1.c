9d492f381bc7a55128a687b3c95b29afdac2565e
ojasvac5-max <ojasvac5@gmail.com>
2025-08-23 17:05:05 +0530

    100DC_1(1)

---
+++ 100DC_1.c
@@ -0,0 +1,12 @@
+// Q1: Write a program to input two numbers and display their sum.
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("ENTER FIRST NUMBER : ");
+    scanf("%d" , &a);
+    printf("ENTER SECOND NUMBER : ");
+    scanf("%d" , &b);
+    printf("THE SUM IS : %d" , a+b );
+    return 0;
+}
