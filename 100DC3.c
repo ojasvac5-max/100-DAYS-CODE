c2c961d28f7e57be5aea67139153fb6a011413e8
ojasvac5-max <ojasvac5@gmail.com>
2025-08-23 19:51:40 +0530

    100DC_3(2)

---
+++ 100DC3.c
@@ -0,0 +1,16 @@
+// Q6: Write a program to swap two numbers using a third variable.
+#include<stdio.h>
+
+int main(){
+    int a , b , i;
+    printf("ENTER THE VALUE OF a:");
+    scanf("%d",&a);
+    printf("ENTER THE VALUE OF b:");
+    scanf("%d",&b);
+    printf("\n BEFORE SWAPPING : a = %d,b =%d\n",a,b);
+    i=a;
+    a=b;
+    b=i;
+    printf("AFTER SWAPPING : a=%d,b=%d",a,b);
+    return 0;
+}
