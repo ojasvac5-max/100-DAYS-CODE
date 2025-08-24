775a7220c26ffeed365cc205d5bdbee31e2c4b2a
ojasvac5-max <ojasvac5@gmail.com>
2025-08-24 18:00:07 +0530

    100DC_2(2)

--- 100DC_4.c
+++ 100DC_4.c
@@ -3,7 +3,7 @@
 
 int main(){
     int r;
-    printf("ENTER THE RADIUS : ");
+    printf("ENTER THE RADIUS :");
     scanf("%d" , &r);
     printf("THE AREA IS :%f" , 3.14*r*r );
     printf("THE CIRCUMFERENCE IS :%f" , 2*3.14*r );
