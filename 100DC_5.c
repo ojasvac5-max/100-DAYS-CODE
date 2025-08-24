b8992166a933da60d324df96f772617808c2e067
ojasvac5-max <ojasvac5@gmail.com>
2025-08-24 18:01:20 +0530

    100DC_3(1)

--- 100DC_5.c
+++ 100DC_5.c
@@ -3,7 +3,7 @@
 
 int main(){
     float t;
-    printf("ENTER THE TEMPERATURE IN CELSIUS : ");
+    printf("ENTER THE TEMPERATURE IN CELSIUS :");
     scanf("%f" , &t);
     printf("THE TEMPERATURE IN FAHRENHEIT IS : %f" , t*(9.0/5.0)+32.0);
     return 0;
