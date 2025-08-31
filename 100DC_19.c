53e74d9b925e1db7dcf77df59ebea49bd50dd1f9
ojasvac5-max <ojasvac5@gmail.com>
2025-08-31 10:17:16 +0530

    100DC_10(1)

---
+++ 100DC_19.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+  int a , b , c;
+    printf("ENTER THE SIDE a :\n" );
+    scanf("%d" , &a);
+printf("ENTER THE SIDE b :\n" );
+    scanf("%d" , &b);
+    printf("ENTER THE SIDE c :\n" );
+    scanf("%d" , &c);
+    if(a==b && b==c){
+        printf("IT IS AN EQUILATERAL TRIANGLE\n");
+    }
+    else if( a == b || b == c || c == a ){
+        printf("IT IS AN ISOSCELES TRIANGLE\n");
+    }else{
+        printf("IT IS A SCALENE TRIANGLE\n");
+    }
+    return 0;
+}
