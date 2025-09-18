341be8b385613d2bf80d93392062eb1db0e845a5
ojasvac5-max <ojasvac5@gmail.com>
2025-09-18 22:39:45 +0530

    100DC_29(2)

--- 100DC_57.c
+++ 100DC_57.c
@@ -3,13 +3,12 @@
 #include<stdio.h>
 
 int main(){
-    int n , i , j , sum = 0;
+    int n , i ,sum = 0;
     printf("ENTER THE NUMBER OF ELEMENTS IN ARRAY : ");
     scanf("%d" , &n);
      int arr[n];
     for(i=0;i<n;i++){
-       scanf("%d" , &j);
-        arr[i] = j;
+       scanf("%d" , &arr[i]);
         sum += arr[i];
     }    
     printf("SUM IS :%d" , sum);
