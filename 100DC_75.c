e637162886909de58f51db1b93ad4e4ad2130b0b
ojasvac5-max <ojasvac5@gmail.com>
2025-10-03 22:16:58 +0530

    100DC_38(1)

---
+++ 100DC_75.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], sum[3][3];
+    int i, j;
+    printf("Enter elements of matrix A (3x3):\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements of matrix B (3x3):\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("Sum of matrices:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
