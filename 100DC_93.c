8cd44d22d958cab4b9173d49b26783cae3b9638f
ojasvac5-max <ojasvac5@gmail.com>
2025-10-06 21:51:50 +0530

    100DC_47(1)

--- 100DC_93.c
+++ 100DC_93.c
@@ -1,3 +1,21 @@
+// Q93: Check if two strings are anagrams of each other.
+
+/*
+Sample Test Cases:
+Input 1:
+listen
+silent
+Output 1:
+Anagrams
+
+Input 2:
+hello
+world
+Output 2:
+Not anagrams
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
